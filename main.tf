1b42649d9c82537e9e84d81aa726dcf52c2828e5
iradukud <iradukuda10@gmail.com>
2025-01-14 08:46:38 -0600

    deployed network security group and security rule

--- main.tf
+++ main.tf
@@ -36,3 +36,27 @@ resource "azurerm_subnet" "dev-subnet" {
   virtual_network_name = azurerm_virtual_network.dev-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "dev-nsg" {
+  name                = "deveploer-security-group"
+  location            = azurerm_resource_group.dev-rg.location
+  resource_group_name = azurerm_resource_group.dev-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "dev-rule" {
+  name                        = "developer-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.dev-rg.name
+  network_security_group_name = azurerm_network_security_group.dev-nsg.name
+}
