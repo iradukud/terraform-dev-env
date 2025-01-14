160039d6c39e20c31e8431eaaa38a519542d8e95
iradukud <iradukuda10@gmail.com>
2025-01-14 10:16:03 -0600

    deployed linux machine

--- main.tf
+++ main.tf
@@ -93,3 +93,35 @@ resource "azurerm_network_interface" "dev-nic" {
     environment = "dev"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "dev-vm" {
+  name                = "dev-vm"
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  location            = azurerm_resource_group.dev-rg.location
+  size                = "Standard_b1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.dev-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/devazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+   tags = {
+    environment = "dev"
+  }
+}
